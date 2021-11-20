2a627f21225c44e3b4b298199dd93b13aa0f3c30
cepuii <cepuii13@gmail.com>
2021-11-20 16:58:00 +0200

    fix jdbc User test

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,17 +1,23 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.TimingRules;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
@@ -27,12 +33,14 @@ public abstract class AbstractServiceTest {
 
     @ClassRule
     public static ExternalResource summary = TimingRules.SUMMARY;
-
+    @Autowired
+    private Environment env;
     @Rule
     public Stopwatch stopwatch = TimingRules.STOPWATCH;
 
     //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
     protected <T extends Throwable> void validateRootCause(Class<T> rootExceptionClass, Runnable runnable) {
+        Assume.assumeFalse(Arrays.stream(env.getActiveProfiles()).anyMatch(env-> env.equals(Profiles.JDBC)));
         assertThrows(rootExceptionClass, () -> {
             try {
                 runnable.run();
